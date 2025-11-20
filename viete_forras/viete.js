300198ebde16ae26b539f06ea5ecea693f9cc706
kissgya <kissgy@kkszki.hu>
2025-11-20 19:21:34 +0100

    Exercise is done.

--- viete_forras/viete.js
+++ viete_forras/viete.js
@@ -1,4 +1,21 @@
 document.getElementById("button1").onclick = () =>{
-    var a = 
-    alert("Hello World!")
+    var a = document.getElementById("inputa").value;
+    var b = document.getElementById("inputb").value;
+    var c = document.getElementById("inputc").value;
+
+    var D = b*b-4*a*c;
+    var eredmeny = "";
+    
+    if (D<0){
+        eredmeny = "Nincs megoldás.";
+    }
+    else if (D==0){
+        eredmeny = "Egy megoldása van.";
+    }
+    else{
+        eredmeny = "Két megoldása van.";
+    }
+
+    document.getElementById("span1").innerHTML = eredmeny;
+    
 }
